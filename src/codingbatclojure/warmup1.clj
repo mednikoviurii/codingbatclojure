d0bccbd1c6126c8f53e28cbb343dbcf64759e2d2
Iurii Mednikov <yuri.mednikov@mednikov.net>
2020-06-21 15:02:54 +0200

    Added from warmup1

--- src/codingbatclojure/warmup1.clj
+++ src/codingbatclojure/warmup1.clj
@@ -22,7 +22,7 @@
 (diff21 21)
 
 (defn nearHundred [n]
-    (println "Near hundred")
+    (println "\nNear hundred")
     (if (or 
     (<= (Math/abs (- 100 n)) 10) 
     (<= (Math/abs (- 200 n)) 10))
@@ -47,3 +47,87 @@
 (monkeyTrouble true true)
 (monkeyTrouble false false)
 (monkeyTrouble true false)
+
+(defn parrotTrouble [hour talking]
+    (println "Parrot trouble")
+        (if (and true? talking (or (< hour 7) (> hour 20)
+        ))
+            (println true)
+            (println false)
+        )
+)
+
+(parrotTrouble 6 true)
+(parrotTrouble 7 true)
+(parrotTrouble 6 false)
+
+
+(defn sumDouble [a b]
+    (println "Sum double")
+    (if (= a b)
+        (println (* 2 (+ a b)))
+        (println (+ a b))
+    )
+)
+
+(sumDouble 1 2)
+(sumDouble 3 2)
+(sumDouble 2 2)
+
+(defn makes10 [a b]
+    (println "Makes10")
+    (if (or 
+            (= 10 (+ a b)) 
+            (or (= 10 a) (= 10 b)
+    ))
+        (println true)
+        (println false)
+
+    )
+
+)
+
+(makes10 9 10)
+(makes10 9 9)
+(makes10 1 9)
+
+(defn isTeen [x]
+    (cond 
+        (and (>= x 13) (<= x 19)) true
+        :else false
+    )
+)
+
+(defn loneTeen [a b]
+    (println "\n Lone teen")
+    (if (
+        and (or (isTeen a) (isTeen b)) 
+            (not= a b)
+    )
+        (println true)
+        (println false)
+    )
+)
+
+(loneTeen 13 99)
+(loneTeen 21 19)
+(loneTeen 13 13)
+
+(defn diffWith10 [x]
+    (Math/abs (- x 10))
+)
+
+(defn close10 [a b]
+    (println "\nClose10")
+    (def diffA (diffWith10 a))
+    (def diffB (diffWith10 b))
+    (cond
+        (< diffA diffB) (println a)
+        (> diffA diffB) (println b)
+        :else (println 0)
+    )
+)
+
+(close10 8 13)
+(close10 13 8)
+(close10 13 7)
